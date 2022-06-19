a9dbc224b4bf7ecadf9de90346fd0cd542738823
samrv2418 <samrv2418@gmail.com>
2022-06-19 19:49:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React and now its fine
         </a>
       </header>
     </div>
